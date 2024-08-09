c8c4da7b52585d66048889c0149e6ce919ba928c
nihilism011 <nihilism011@gmail.com>
2024-08-09 18:08:34 +0900

    초반 세팅중

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <v-btn color="primary">Vuetify 버튼</v-btn>
+      <v-btn color="primary"></v-btn>
+      <v-btn depressed color="pink">click me</v-btn>
+      <HelloWorld />
     </v-main>
+  </v-app>
+  <v-app>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
@@ -19,5 +25,5 @@ export default {
   data: () => ({
     //
   }),
-}
+};
 </script>
